09385b605f7f2a9551baa47ad19e1bcc948db444
Alex <alexandergun16@gmail.com>
2024-06-17 18:11:08 +0700

    Add Log-In Button Animation, Improve inputs placeholders, Create Sign-Up Page

--- src/SignUp.tsx
+++ src/SignUp.tsx
@@ -11,10 +11,10 @@ function App() {
         <form action="" method="get">
           <input type="text" id="input-username" placeholder="Username"></input>
           <input type="password" id="input-password" placeholder="Password"></input>
-          <Button btnType="btn-login">Log In</Button>
+          <Button btnType="btn-signup">Sign Up</Button>
         </form>
         <p>Don't have An Account Yet?</p>
-        <a href="" className="sign-up-link">
+        <a href="" className="log-in-link">
           <strong>Sign Up Here</strong>
         </a>
       </div>
