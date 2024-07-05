a521aec72a1a7871668f7045b66e3672ee14ec52
peter <peter_krn@hotmail.com>
2024-07-05 20:37:46 +0700

    fixed some singleton pattern implementations.

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -1,114 +1,64 @@
-import { createPool, FieldPacket, OkPacket, PoolConnection, ResultSetHeader } from "mysql2/promise";
-import dotenv from 'dotenv';
+import {
+  createPool,
+  Pool,
+  PoolConnection,
+  FieldPacket,
+  OkPacket,
+  ResultSetHeader,
+} from "mysql2/promise";
+import dotenv from "dotenv";
 
 dotenv.config();
 const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
 
 class InitDB {
-    private static instance: InitDB;
-    private connection: PoolConnection;
-    private transaction: Transaction | null;
+  private static instance: InitDB;
+  private static pool: Pool;
+  private connection: PoolConnection;
 
-    private constructor(connection: PoolConnection) {
-        this.connection = connection;
-        this.transaction = null;
+  private constructor(connection: PoolConnection) {
+    this.connection = connection;
+  }
+
+  public static async getInstance(): Promise<InitDB> {
+    if (!InitDB.pool) {
+      InitDB.pool = createPool({
+        host: DB_HOST || "localhost",
+        user: DB_USER || "root",
+        password: DB_PASSWORD || "",
+        database: DB_NAME || "hobigowes",
+        waitForConnections: true,
+        connectionLimit: 10,
+        queueLimit: 0,
+      });
     }
 
-    public static async getInstance(): Promise<InitDB> {
-        if (!InitDB.instance) {
-            const connection = await createPool({
-                host: DB_HOST || 'localhost',
-                user: DB_USER || 'root',
-                password: DB_PASSWORD || '',
-                database: DB_NAME || 'hobigowes',
-                waitForConnections: true,
-                connectionLimit: 10,
-                queueLimit: 0
-            }).getConnection();
-            InitDB.instance = new InitDB(connection);
-        }
-        return InitDB.instance;
-    }
+    const connection = await InitDB.pool.getConnection();
+    return new InitDB(connection);
+  }
 
-    public async beginTransaction(): Promise<void> {
-        if (!this.transaction) {
-            this.transaction = Transaction.getInstance(this.connection);
-            await this.transaction.beginTransaction();
-        }
-    }
+  public async query(
+    query: string,
+    values?: any[]
+  ): Promise<[any, FieldPacket[] | OkPacket[] | ResultSetHeader[]]> {
+    return this.connection.execute(query, values);
+  }
 
-    public async commit(): Promise<void> {
-        if (this.transaction) {
-            await this.transaction.commit();
-            this.transaction = null;
-        }
-    }
+  public release(): void {
+    this.connection.release();
+  }
 
-    public async rollback(): Promise<void> {
-        if (this.transaction) {
-            await this.transaction.rollback();
-            this.transaction = null;
-        }
-    }
+  public async beginTransaction(): Promise<void> {
+    await this.connection.beginTransaction();
+  }
 
-    public async query(query: string, values?: any[]): Promise<[any, FieldPacket[] | OkPacket[] | ResultSetHeader[]]> {
-        if (this.transaction) {
-            return this.transaction.query(query, values);
-        } else {
-            return this.connection.execute(query, values);
-        }
-    }
+  public async commit(): Promise<void> {
+    await this.connection.commit();
+  }
 
-    public release(): void {
-        if (this.transaction) {
-            this.transaction.rollback(); // Rollback the transaction if it's still open
-            this.transaction = null;
-        }
-        this.connection.release();
-    }
-}
-
-class Transaction {
-    private static instance: Transaction | null = null;
-    private connection: PoolConnection;
-    private inTransaction: boolean;
-
-    private constructor(connection: PoolConnection) {
-        this.connection = connection;
-        this.inTransaction = false;
-    }
-
-    public static getInstance(connection: PoolConnection): Transaction {
-        if (!Transaction.instance) {
-            Transaction.instance = new Transaction(connection);
-        }
-        return Transaction.instance;
-    }
-
-    public async beginTransaction(): Promise<void> {
-        if (!this.inTransaction) {
-            await this.connection.beginTransaction();
-            this.inTransaction = true;
-        }
-    }
-
-    public async commit(): Promise<void> {
-        if (this.inTransaction) {
-            await this.connection.commit();
-            this.inTransaction = false;
-        }
-    }
-
-    public async rollback(): Promise<void> {
-        if (this.inTransaction) {
-            await this.connection.rollback();
-            this.inTransaction = false;
-        }
-    }
-
-    public async query(query: string, values?: any[]): Promise<[any, FieldPacket[] | OkPacket[] | ResultSetHeader[]]> {
-        return this.connection.execute(query, values);
-    }
+  public async rollback(): Promise<void> {
+    await this.connection.rollback();
+  }
 }
 
 export default InitDB;
--- backend/src/models/post.ts
+++ backend/src/models/post.ts
@@ -1,91 +1,129 @@
-import pool from "../db";
+import InitDB from "../database";
+
 enum bike_type {
-    'mountain bike',
-    'road bike',
-    'touring bike',
-    'bmx'
+  "mountain bike",
+  "road bike",
+  "touring bike",
+  "bmx",
 }
 
 enum status {
-    'available',
-    'sold out'
+  "available",
+  "sold out",
 }
 
 interface Post {
-    id: number;
-    title: string;
-    bike_type: bike_type;
-    description: string;
-    price: number;
-    city: string;
-    province: string;
-    upload_date: string | Date;
-    stok: number;
-    status: status;
-    user_id: number;
-    url: string;
+  id: number;
+  title: string;
+  bike_type: bike_type;
+  description: string;
+  price: number;
+  city: string;
+  province: string;
+  upload_date: string | Date;
+  stok: number;
+  status: status;
+  user_id: number;
+  url: string;
 }
 
 export const getAllPosts = async (): Promise<Array<Post> | null> => {
-    const conn = await pool.getConnection();
-    // const posts = Array<Post>;
-    try {
-        const [rows] = await conn.query("SELECT * FROM post JOIN image ON post.id = image.post_id");
-        conn.release();
+  const conn = await InitDB.getInstance();
+  // const posts = Array<Post>;
+  try {
+    const [rows] = await conn.query(
+      "SELECT * FROM post JOIN image ON post.id = image.post_id"
+    );
+    conn.release();
 
-        return rows as Array<Post> // Harus di cek lagi bener apa engga
-    } catch (error) {
-        console.error("Error getting all posts");
-        conn.release();
-        return null;
-    }
-}
+    return rows as Array<Post>; // Harus di cek lagi bener apa engga
+  } catch (error) {
+    console.error("Error getting all posts");
+    conn.release();
+    return null;
+  }
+};
 
-export const createPost = async (title: string, bike_type: bike_type, description: string, price: number, city: string, province: string, upload_date: string, stok: number, status: status, user_id: number): Promise<void> => {
-    const conn = await pool.getConnection();
-    try {
-        await conn.query("INSERT INTO post (title, bike_type, description, price, city, province, upload_date, stok, status, user_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", [title, bike_type, description, price, city, province, upload_date, stok, status, user_id]);
-        conn.release();
-    } catch (error) {
-        console.error("Unexpected Error Occured");
-        conn.release();
-        throw error;
-    }
-}
+export const createPost = async (
+  title: string,
+  bike_type: bike_type,
+  description: string,
+  price: number,
+  city: string,
+  province: string,
+  upload_date: string,
+  stok: number,
+  status: status,
+  user_id: number
+): Promise<void> => {
+  const conn = await InitDB.getInstance();
+  try {
+    await conn.query(
+      "INSERT INTO post (title, bike_type, description, price, city, province, upload_date, stok, status, user_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+      [
+        title,
+        bike_type,
+        description,
+        price,
+        city,
+        province,
+        upload_date,
+        stok,
+        status,
+        user_id,
+      ]
+    );
+    conn.release();
+  } catch (error) {
+    console.error("Unexpected Error Occured");
+    conn.release();
+    throw error;
+  }
+};
 
 export const getPostByID = async (id?: number): Promise<Post | null> => {
-    const conn = await pool.getConnection();
-    try {
-        const [rows] = await conn.query("SELECT id, title, bike_type, description, price, city, province, upload_date, stok, status, user_id FROM post WHERE id = ?", [id]);
-        conn.release();
+  const conn = await InitDB.getInstance();
+  try {
+    const [rows] = await conn.query(
+      "SELECT id, title, bike_type, description, price, city, province, upload_date, stok, status, user_id FROM post WHERE id = ?",
+      [id]
+    );
+    conn.release();
 
-        if (Array.isArray(rows) && rows.length > 0) {
-            return rows[0] as Post;
-        } else {
-            return null;
-        }
-    } catch (error) {
-        console.error("Error finding post by id:", error);
-        conn.release();
-        return null;
+    if (Array.isArray(rows) && rows.length > 0) {
+      return rows[0] as Post;
+    } else {
+      return null;
     }
-}
+  } catch (error) {
+    console.error("Error finding post by id:", error);
+    conn.release();
+    return null;
+  }
+};
 
-export const searchPosts = async (keyword: string): Promise<Array<Post> | null> => {
-    const conn = await pool.getConnection();
-    try {
-        const query = `
+export const searchPosts = async (
+  keyword: string
+): Promise<Array<Post> | null> => {
+  const conn = await InitDB.getInstance();
+  try {
+    const query = `
             SELECT * FROM post
             WHERE title LIKE ? OR description LIKE ? OR city LIKE ? OR province LIKE ?
         `;
-        const [rows] = await conn.query(query, [`%${keyword}%`, `%${keyword}%`, `%${keyword}%`, `%${keyword}%`]);
-        conn.release();
-        return rows as Array<Post>;
-    } catch (error) {
-        console.error("Error searching posts", error);
-        conn.release();
-        return null;
-    }
-}
+    const [rows] = await conn.query(query, [
+      `%${keyword}%`,
+      `%${keyword}%`,
+      `%${keyword}%`,
+      `%${keyword}%`,
+    ]);
+    conn.release();
+    return rows as Array<Post>;
+  } catch (error) {
+    console.error("Error searching posts", error);
+    conn.release();
+    return null;
+  }
+};
 
 export default Post;
